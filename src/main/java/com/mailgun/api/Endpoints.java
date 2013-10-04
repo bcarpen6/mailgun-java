5178ea2c2bb083858eea9c6ae120c65836ec859b
Brett Carpenter <bcarpenter@thrillhouse.local>
2013-10-04 10:55:01 -0400

    change endpoints from interface to class

--- src/main/java/com/mailgun/api/Endpoints.java
+++ src/main/java/com/mailgun/api/Endpoints.java
@@ -1,26 +1,29 @@
 package com.mailgun.api;
 
-public interface Endpoints {
-	String MESSAGES = "/messages";
-	String MESSAGES_MIME = "/messages.mime";
-	String MESSAGE_DETAILS = "/messages/%s";
-	String UNSUBSCRIBES	= "/unsubscribes";
-	String UNSUBSCRIBE_DETAILS = "/unsubscribes/%s";
-	String COMPLAINTS = "/complaints";
-	String COMPLAINT_DETAILS = "/complaints/%s";
-	String BOUNCES = "/bounces";
-	String BOUNCE_DETAILS = "/bounces/%s";
-	String STATS = "/stats";
-	String TAGS	= "/tags/%s";
-	String EVENTS = "/events";
-	String ROUTES = "/routes";
-	String ROUTE_DETAILS = "/routes/%s";
-	String MAILBOXES = "/mailboxes";
-	String MAILBOX_DETAILS = "/mailboxes/%s";
-	String CAMPAIGNS = "/campaigns";
-	String CAMPAIGN_DEATILS	= "/campaigns/%s";
-	String LISTS = "/lists";
-	String LIST_DETAILS = "/lists/%s";
-	String VALIDATE	= "/address/validate";
-	String VALIDATE_PARSE = "/address/parse";
+public final class Endpoints {
+
+	private Endpoints() {}
+
+	public static final String MESSAGES = "/messages";
+	public static final String MESSAGES_MIME = "/messages.mime";
+	public static final String MESSAGE_DETAILS = "/messages/%s";
+	public static final String UNSUBSCRIBES	= "/unsubscribes";
+	public static final String UNSUBSCRIBE_DETAILS = "/unsubscribes/%s";
+	public static final String COMPLAINTS = "/complaints";
+	public static final String COMPLAINT_DETAILS = "/complaints/%s";
+	public static final String BOUNCES = "/bounces";
+	public static final String BOUNCE_DETAILS = "/bounces/%s";
+	public static final String STATS = "/stats";
+	public static final String TAGS	= "/tags/%s";
+	public static final String EVENTS = "/events";
+	public static final String ROUTES = "/routes";
+	public static final String ROUTE_DETAILS = "/routes/%s";
+	public static final String MAILBOXES = "/mailboxes";
+	public static final String MAILBOX_DETAILS = "/mailboxes/%s";
+	public static final String CAMPAIGNS = "/campaigns";
+	public static final String CAMPAIGN_DEATILS	= "/campaigns/%s";
+	public static final String LISTS = "/lists";
+	public static final String LIST_DETAILS = "/lists/%s";
+	public static final String VALIDATE	= "/address/validate";
+	public static final String VALIDATE_PARSE = "/address/parse";
 }
